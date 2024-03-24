9dd6b7448c4dbeaf1c4f05726f9b71753a67f193
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-03-24 10:55:21 +0300

    Update loop.py

--- AnonXMusic/plugins/admins/loop.py
+++ AnonXMusic/plugins/admins/loop.py
@@ -8,7 +8,7 @@ from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["loop", "cloop"]) & filters.group & ~BANNED_USERS)
+@app.on_message(filters.command(["tekrar"]) & filters.group & ~BANNED_USERS)
 @AdminRightsCheck
 async def admins(cli, message: Message, _, chat_id):
     usage = _["admin_17"]
@@ -17,12 +17,10 @@ async def admins(cli, message: Message, _, chat_id):
     state = message.text.split(None, 1)[1].strip()
     if state.isnumeric():
         state = int(state)
-        if 1 <= state <= 10:
+        if 1 <= state <= float("inf"):  # Sınırsız olarak değiştirdik
             got = await get_loop(chat_id)
             if got != 0:
                 state = got + state
-            if int(state) > 10:
-                state = 10
             await set_loop(chat_id, state)
             return await message.reply_text(
                 text=_["admin_18"].format(state, message.from_user.mention),
@@ -30,13 +28,13 @@ async def admins(cli, message: Message, _, chat_id):
             )
         else:
             return await message.reply_text(_["admin_17"])
-    elif state.lower() == "enable":
-        await set_loop(chat_id, 10)
+    elif state.lower() == "acik":
+        await set_loop(chat_id, float("inf"))  # Sınırsız olarak değiştirdik
         return await message.reply_text(
-            text=_["admin_18"].format(state, message.from_user.mention),
+            text=_["admin_18"].format("sınırsız", message.from_user.mention),
             reply_markup=close_markup(_),
         )
-    elif state.lower() == "disable":
+    elif state.lower() == "kapali":
         await set_loop(chat_id, 0)
         return await message.reply_text(
             _["admin_19"].format(message.from_user.mention),
