2853dd348c084243810201d875715d6593dc347e
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-03-24 10:26:48 +0300

    Update logger.py

--- AnonXMusic/plugins/sudo/logger.py
+++ AnonXMusic/plugins/sudo/logger.py
@@ -6,17 +6,17 @@ from AnonXMusic.utils.database import add_off, add_on
 from AnonXMusic.utils.decorators.language import language
 
 
-@app.on_message(filters.command(["logger"]) & SUDOERS)
+@app.on_message(filters.command(["log"]) & SUDOERS)
 @language
 async def logger(client, message, _):
     usage = _["log_1"]
     if len(message.command) != 2:
         return await message.reply_text(usage)
     state = message.text.split(None, 1)[1].strip().lower()
-    if state == "enable":
+    if state == "acik":
         await add_on(2)
         await message.reply_text(_["log_2"])
-    elif state == "disable":
+    elif state == "kapali":
         await add_off(2)
         await message.reply_text(_["log_3"])
     else:
