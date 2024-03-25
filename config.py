8595803a800bb36597eaea3b6512b2fd19227983
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-03-25 12:05:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,10 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/MehmetAtes21/Pi",
 )
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BotsDuyuru")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BotsDestek")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
-STRING1 = getenv("STRING_SESSION", "BAAzn_141ulDy5E_zn7HlrnPwVs7srvTIvPMZea-9rt4pVesbYxR1MkIroY2cZWOMwemESZh1WkfxN9mq8Y2cuTxxe0pfk8hH2U9koUV0vlaoPFTVu1hzPug9i77jQpaV0GfT0wjTNhDLMGkKBzTnmx6aHUaP2XtLawC-Y_i40kj-BVcjb6_OyCVkr0vDJN2f9wQSn94qVXmEhzCwKNH6t8SL6dpG9jrOTrQTMvqGtPQd5TcE8g-UOO8Dj4SiLd0KVdgmCQjbqbqOvREBqaSCi_hguVRqaYhKED8E1QNeCJiKdvmSj3KQpRjlqWdFYJxtj8f-6hM2y8Ox9J2GUrC70fmAAAAAaR10EAA")
+STRING1 = getenv("STRING_SESSION", "BABiHameZE0Y_IsXM2GgZ6Tqk9rxl1-KVU2PoBG6AtZP1eyC7a_Adf_L6qrlHLCVWwMx8ZIfJQVEHTGJlkWTOny_Fh0zd7x2k-8X3OqphrdhR1KaGG9vPcm65o0cL34MDizaT9aQ-BEY_U9yOs-CewC6hTilLVOGgPscPpRb4GVywFH_rgwwNgVT2gTRfvqfWHHLQbZM5gWgcXQxo2dX4du6wR1LD1RCtNJ138NrhgaCeztboLMdzxdZa-GRKmI351MGwohuIiTTuDkE4515YWmUBPgb2lGAaYN8JSDHqrRWe_ubdpjsyTKSbgJm-td_cG8tQxrHzsxG9qRh8Kj1xbL7AAAAAaR10EAA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
