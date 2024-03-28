8793d56a99d48a80c183c7e5c4051f56dc9eb879
MehmetAtes21 <99514614+MehmetAtes21@users.noreply.github.com>
2024-03-28 04:29:35 +0300

    Update play.py

--- AnonXMusic/utils/decorators/play.py
+++ AnonXMusic/utils/decorators/play.py
@@ -36,7 +36,7 @@ def PlayWrapper(command):
                 [
                     [
                         InlineKeyboardButton(
-                            text="ʜᴏᴡ ᴛᴏ ғɪx ?",
+                            text="𝖭𝖺𝗌ı𝗅 𝖣𝗎̈𝗓𝖾𝗅𝗍𝗂𝗅𝗂𝗋 ?",
                             callback_data="AnonymousAdmin",
                         ),
                     ]
@@ -47,7 +47,7 @@ def PlayWrapper(command):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    text=f"{app.mention} bakım altında, nedenini öğrenmek için <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> adresini ziyaret edin.",
                     disable_web_page_preview=True,
                 )
 
