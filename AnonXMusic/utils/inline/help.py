bfa9bcc6a6baf9ba6c108a99d34ce1154da939f1
Meyit47zade <166849634+Meyit47zade@users.noreply.github.com>
2024-07-17 23:24:12 +0300

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -34,7 +34,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             ],
             [
                  InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-                 InlineKeyboardButton(text="✚  ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ  ", url="https://www.youtube.com/@EpikTv87"),
+                 InlineKeyboardButton(text="✚ BOT SAHİBİ  ", url="https://t.me/Meyitzade"),
             ],
             mark,
         ]
@@ -63,7 +63,7 @@ def private_help_panel(_):
                 text=_["S_B_4"],
                 url=f"https://t.me/{app.username}?start=help",
             ),
-            InlineKeyboardButton(text="✚ ᴋᴀʏɴᴀᴋ ᴋᴏᴅᴜ ✚", url="https://www.youtube.com/@EpikTv87"),
+            InlineKeyboardButton(text="✚ BOT SAHİBİ", url="https://t.me/Meyitzade"),
         ],
     ]
     return buttons
